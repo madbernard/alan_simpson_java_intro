bd1419fa1c1934fb41560b30a825b23c763a5e7a
Mad Bernard <github@z-amber.com>
2022-10-21 14:48:52 -0700

    Add HelloWorld code

---
+++ HelloWorld1/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello, world");
+        System.out.print("Hello, df woher sodafu askdfk yawe asdn");
+        System.out.println("Helloooooo");
+    }
+}
