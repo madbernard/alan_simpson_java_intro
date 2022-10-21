77afa205b276d9d2be118cb4b74a18f650356d19
Mad Bernard <github@z-amber.com>
2022-10-21 15:19:31 -0700

    Send all primitives to System out

---
+++ DisplayAllPrimitives/DisplayAllPrimitives.java
@@ -0,0 +1,14 @@
+
+public class DisplayAllPrimitives {
+    public static void main (String[] args) {
+        System.out.println(33); // Int
+        System.out.println(339922001144556677L); // Long
+        System.out.println('a'); // Char
+        System.out.println(33.0897); // Double
+        System.out.println(450.37F); // Float
+        System.out.println(true); // Boolean
+        System.out.println(0b00010); // Binary
+        System.out.println(0xab1e); // Hex
+        System.out.println("String\nFTW"); // String
+    }
+}
